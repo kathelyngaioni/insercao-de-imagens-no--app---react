804d96ecdafa2e9856f54aa515a78848156d6adf
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2022-11-17 09:32:33 +0000

    Insercao de imagens no App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import './App.css';
+import React from 'react'
+import Foto from './componentes/imgs/foto.n.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+    <section>
+      <header>
+        <p>{'Minha foto: '}</p>
       </header>
-    </div>
-  );
+      <div>
+        <img src={Foto}/>
+        <img src ='logo192.png'/>
+      </div>
+    </section>
+  )
 }
 
 export default App;
